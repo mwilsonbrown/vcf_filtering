8ca6c03ce06ef3f855167607aded7b2c68c3f8b4
mwilsonbrown <wils1582@msu.edu>
2024-10-16 10:41:25 -0400

    split population file

--- ld_decay/noodling_LD_decay.sh
+++ ld_decay/noodling_LD_decay.sh
@@ -1,29 +1,27 @@
-# just  noodling around to mkae LD decay plots so I can choose biologically informed window sizes when doing windowed  genomic analyses
+# Making LD decay plots so I can choose biologically informed window sizes when doing windowed  genomic analyses
 # M Wilson Brown
-# September 11, 2024
+# October 16, 2024
 
 ##modules
 #module purge
 #module load PLINK/2.00a3.7-gfbf-2023a
 #
 ## setting up VCF variables 
-VCF=/mnt/home/wils1582/allSites_CBP_final.filtered.vcf.gz
-## run PLINK on Capsella bursa-pastoris vcf only on one chromosome
-#plink2 --vcf $VCF --double-id --allow-extra-chr \
-#	--set-missing-var-ids @:# \
-#	--chr 'jlSCF_10' \
-#	--thin 0.1 \
-#	--r2-unphased gz \
-#	--ld-window 100 \
-#	--ld-window-kb 1000 \
-#	--ld-window-r2 0 \
-#	--make-bed --out cbp_LD_JLv4
-## The above does not recognizze its own flags and throws an error
-# advice online is to just use PLINK1.9 when this happens so I guess I will do that (silly)
+#VCF=/mnt/home/wils1582/allSites_CBP_final.filtered.vcf.gz
+
+# I plan on calculating LD on the different subpopulations separately, so I need the population file for that
+# Load population file
+POPFILE=/mnt/home/wils1582/capsella_population_structure/cbp_pop_str.txt
+
+# use awk to split them by the third column
+awk '$3 == N_Europe' $POPFILE > n_europe
+
+
 module purge
 module load PLINK/1.9b_6.21-x86_64 
 
-plink --vcf $VCF --double-id --allow-extra-chr \
+plink --vcf $VCF --double-id \
+        --allow-extra-chr \
         --set-missing-var-ids @:# \
         --thin 0.1 -r2 gz \
         --ld-window 100 \
