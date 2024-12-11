fc22a1635f21aedc8bc4cbe2a94850578d396c9b
Maya Wilson Brown <wils1582@msu.edu>
2024-12-11 14:54:21 -0500

    update to SLURM header to get it to run

--- recombination_test/Cbp_relernn_test_pipeline.sh
+++ recombination_test/Cbp_relernn_test_pipeline.sh
@@ -1,22 +1,23 @@
 #!/bin/bash --login
-#
 #SBATCH --job-name=ReLERNN
 #SBATCH --constraint=amd20
-#SBATCH --gpus=v100:4
-#SBATCH --ntasks=1
-#SBATCH --nodes=10
-#SBATCH --cpus-per-task=1
-#SBATCH --time=0-50:00:00
-#SBATCH --partition=josephsnodes
-#SBATCH --account=josephsnodes
-#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=7-00:00:00
+#SBATCH --mem-per-cpu=16G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wils1582@msu.edu
 #SBATCH --output=/mnt/scratch/wils1582/slurm/%x-%A.SLURMout
+#SBATCH --partition=josephsnodes
+#SBATCH --account=josephsnodes
 # ReLERNN on VCF
 # DEC 2, 2024
 # Maya Wilson Brown
 # constrained job request to amd20 nodes because that is the node I built tensorflow on
+# output information about how this job is running using bash commands
+echo "This job is running on $HOSTNAME on `date`"
+
 
 ######## SETUP
 export PATH=/mnt/home/wils1582/miniconda3/bin:$PATH
@@ -25,7 +26,7 @@ source /mnt/home/wils1582/miniconda3/bin/activate relernn
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lib/:/lib64/:$CONDA_PREFIX/lib/:$CONDA_PREFIX/lib/python3.9/site-packages/tensorrt
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CONDA_PREFIX/lib
 
-cd /mnt/home/wils1582/ReLERNN
+cd /mnt/home/wils1582/ReLERNN/examples/
 
 ######## PIPELINE
 SIMULATE="ReLERNN_SIMULATE"
