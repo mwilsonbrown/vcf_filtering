fa16a70ee881e176c1f1c9d997e568b9c56badd5
mwilsonbrown <wils1582@msu.edu>
2025-04-01 11:12:58 -0400

    add sample renaming

--- 001_vcf_preprocess.sh
+++ 001_vcf_preprocess.sh
@@ -4,7 +4,7 @@
 
 # Variables
 PREFIX=CBP2_CR_CG_on_CBP-CG
-VAR_VCF=/mnt/scratch/wils1582/BIG_vcfs/merged_final_CBP2_CR_CG_on_CBP-CG.v.vcf
+RAW_VCF=/mnt/scratch/wils1582/BIG_vcfs/merged_final_CBP2_CR_CG_on_CBP-CG.v.vcf
 OUTDIR=/mnt/home/wils1582/vcf_filtering/individuals
 WORKDIR=/mnt/scratch/wils1582
 #ALLSITES_VCF=/mnt/research/josephslab/Adrian/CBP_NYC_JLv4/CBP_JLv4_v_CBP.merged.v.all.vcf
@@ -19,7 +19,10 @@ module load BCFtools/1.19-GCC-13.2.0
 module load  R/4.3.3-gfbf-2023b
 
 # get list of samples in each VCF
-bcftools query -l $VAR_VCF > pre_samples.txt
+bcftools query -l $RAW_VCF > pre_samples.txt
 
 # Run R script to generate files for subsetting vcf
 Rscript 001a_check_raw_vcf.R pre_samples.txt "$PREFIX"
+
+# reneame samples
+bcftools reheader -s "$OUTDIR"/"$PREFIX"_new_names.txt $RAW_VCF -Ov -o "$PREFIX"_rmF2.vcf
