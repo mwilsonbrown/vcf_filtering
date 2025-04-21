27eb63658a7bdf91cbdd666dcc1d9de4ed186c76
Maya Wilson Brown <wils1582@msu.edu>
2025-04-21 13:59:40 -0400

    updates to thirs step for filtering multispecies VCF on minor allele freq

--- 003_remove_inds.sh
+++ 003_remove_inds.sh
@@ -19,6 +19,7 @@
 # Variables
 PREFIX=CBP2_CR_CG_on_CBP-CG
 WORKDIR=/mnt/scratch/wils1582/"$PREFIX"_filtering
+FINALDIR=/mnt/research/josephslab/Maya/capsella/vcf/filtered
 
 # change directory
 cd $WORKDIR
@@ -75,72 +76,11 @@ plink2 --vcf CG.vcf \
   --allow-extra-chr \
   --double-id \
   --out CG_split
-## In R
-## Read in allele frquency file caluculated with PLINK
-#cbp_frq <- read.delim("./CBP_split.afreq")
-## Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
-#cbp_var <- cbp_frq[which(cbp_frq$REF_FREQ != 1 & cbp_frq$ALT_FREQ !=1),]
-## of the variant sites, make a list of those where either the reference or alternate allele frequecy is below the threshold
-#cbp_rm <- cbp_var[which(cbp_var$REF_FREQ < 0.025 | cbp_var$ALT_FREQ < 0.025),]
-#
-## do the same for Capsella rubella
-#cr_frq <- read.delim("./CR_split.afreq")
-## Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
-#cr_var <- cr_frq[which(cr_frq$REF_FREQ != 1 & cr_frq$ALT_FREQ !=1),]
-## of the variant sites, make a list of those where either the reference or alternate allele frequecy is below the threshold
-#cr_rm <- cr_var[which(cr_var$REF_FREQ < 0.025 | cr_var$ALT_FREQ < 0.025),]
-#
-## and the same for Capsella grandiflora but the MAF cut off is more stringent (0.05)
-#cg_frq <- read.delim("./CG_split.afreq")
-## Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
-#cg_var <- cg_frq[which(cg_frq$REF_FREQ != 1 & cg_frq$ALT_FREQ !=1),]
-## of the variant sites, make a list of those where either the reference or alternate allele frequecy is below the threshold
-#cg_rm <- cg_var[which(cg_var$REF_FREQ < 0.05 | cg_var$ALT_FREQ < 0.05),]
-#
-## concatenate all removals together
-#rm_sites <- rbind(cbp_rm[1:3], cr_rm[1:3], cg_rm[1:3])
-#
-## remove the duplicates
-#rm_sites_nodups <- rm_sites[!duplicated(rm_sites),]
-#
-## Write to file
-#write.table(rm_sites_nodups[1:2], file = "remove_MAF.txt", sep = "\t", header = F, quote = F, row.names = F, col.names = F)
-#
-#  
-## Then, have to recalculate the Allele count, Allele Frequency, and Allele Number on each separately
-## before filtering on MAF
-## Not filtering NP or C. orientalis because there are not that many samples
-## I could probably pipe the fill into the view command but it's fine
-#bcftools +fill-tags CBP.vcf -Oz -o CBP_recalc.vcf.gz -- -t AC,AF,AN
-#bcftools +fill-tags CR.vcf -Oz -o CR_recalc.vcf.gz -- -t AC,AF,AN
-#bcftools +fill-tags CG.vcf -Oz -o CG_recalc.vcf.gz -- -t AC,AF,AN
-#
-## Filter on MAF for each
-## The definition of AC and AF in BCFTools is for the alternate allele only
-## I only want to filter on Minor Allele Frequecy on sites where there is a minor allele at all
-#
-## I cannot figure out how to fix this right now so I think I am just going to use PLINK2
-#plink2 --vcf CBP.vcf \
-#--freq cols=chrom,pos,id,ref,alt,reffreq,altfreq,nobs \
-#--set-all-var-ids @:# \
-#--allow-extra-chr \
-#--out CBP_split
-## C. grandiflora threshold higher bc outcrosser and eQTLs from EJ paper
-## Not filtering NP or C. orientalis because there are not that many samples
-#bcftools view --min-af 0.025:minor CBP_recalc.vcf.gz -o CBP_maf.vcf.gz -Oz
-#bcftools view --min-af 0.025:minor CR_recalc.vcf.gz -o CR_maf.vcf.gz -Oz
-#bcftools view --min-af 0.05:minor CG_recalc.vcf.gz -o CG_maf.vcf.gz -Oz
-#
-## bgzip the other two vcfs
-#bgzip NP.vcf
-#bgzip CO.vcf
-#
-## Index all of them
-#tabix CBP_maf.vcf.gz 
-#tabix CR_maf.vcf.gz 
-#tabix CG_maf.vcf.gz 
-#tabix CO.vcf.gz 
-#tabix NP.vcf.gz
-## merge all the VCFs back together; one final time, we recalculate the tags
-#bcftools merge CBP_maf.vcf.gz CR_maf.vcf.gz CG_maf.vcf.gz CO.vcf.gz NP.vcf.gz -Ou | bcftools +fill-tags -Oz -o CBP_CRCGCONP_maf_final_filtered.vcf.gz -- -t AC,AF,AN
-#
+
+## C. grandiflora MAF threshold higher bc outcrosser and eQTLs from EJ paper
+Rscript ~/vcf_filtering/003b_multipop_calc_MAF.R $WORKDIR
+
+# Use bcftools to filter on MAF
+bcftools view --targets-file ^remove_MAF.txt "$WORKDIR"/"$PREFIX"_qualfiltered.vcf.gz \
+	-Oz -o "$FINALDIR"/"$PREFIX"_qualMAF_snps_only.vcf.gz
+tabix "$FINALDIR"/"$PREFIX"_qualMAF_snps_only.vcf.gz
--- 003b_calc_MAF.R
+++ 003b_calc_MAF.R
@@ -1,39 +1,58 @@
 # Calculating multispecies minor allele frequency
-# Read in allele frquency file caluculated with PLINK
-cbp_frq <- read.delim("./CBP_split.afreq")
+
 
 # Because I filtered to biallelic sites, there should only be 2 alleles--REF and ALT
 # Therefore, the minor allele is just the less common one
+# I also decided that I should only remove minor alleles that are below the minor allele threshold in all three species
+# because those are the one that are actually rare and may represent errors
 
-# also, I dont really care about all the other minor alelles, just the ones that are very very rare
+# args[1] is working directory
+args = commandArgs(trailingOnly=TRUE)
 
-cbp_rare <- cbp_frq[which]
+workdir <- as.character(args[1])
+# will work on non hard-coding the input filenames later
+
+# Read in allele frquency file caluculated with PLINK
+cbp_frq <- read.delim(paste0(workdir,"/CBP_split.afreq"))
+# do the same for Capsella rubella
+cr_frq <- read.delim(paste0(workdir,"/CR_split.afreq"))
+# and the same for Capsella grandiflora but the MAF cut off is more stringent (0.05)
+cg_frq <- read.delim(paste0(workdir,"/CG_split.afreq"))
 
 
 # Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
 cbp_var <- cbp_frq[which(cbp_frq$REF_FREQ != 1 & cbp_frq$ALT_FREQ !=1),]
-# of the variant sites, make a list of those where either the reference or alternate allele frequency is below the threshold
-cbp_rm <- cbp_var[which(cbp_var$REF_FREQ < 0.025 | cbp_var$ALT_FREQ < 0.025),]
-
-# do the same for Capsella rubella
-cr_frq <- read.delim("./CR_split.afreq")
 # Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
 cr_var <- cr_frq[which(cr_frq$REF_FREQ != 1 & cr_frq$ALT_FREQ !=1),]
-# of the variant sites, make a list of those where either the reference or alternate allele frequency is below the threshold
-cr_rm <- cr_var[which(cr_var$REF_FREQ < 0.025 | cr_var$ALT_FREQ < 0.025),]
-
-# and the same for Capsella grandiflora but the MAF cut off is more stringent (0.05)
-cg_frq <- read.delim("./CG_split.afreq")
 # Only select variant sites (neither the reference nor alternate allele had a frequency of 1)
 cg_var <- cg_frq[which(cg_frq$REF_FREQ != 1 & cg_frq$ALT_FREQ !=1),]
-# of the variant sites, make a list of those where either the reference or alternate allele frequency is below the threshold
-cg_rm <- cg_var[which(cg_var$REF_FREQ < 0.05 | cg_var$ALT_FREQ < 0.05),]
 
-# concatenate all removals together
-rm_sites <- rbind(cbp_rm[1:3], cr_rm[1:3], cg_rm[1:3])
 
-# remove the duplicates
-rm_sites_nodups <- rm_sites[!duplicated(rm_sites),]
+# actually, also for this to work, the SAME ALLELE needs to be below the MAF threshold in all three species
+# For example: Chr9:139995, Reference allele A is below the MAF threshold in C. grandiflora 
+# but alternate allele T is below the MAF threshold in C. bursa-pastoris and C. rubella
+# What I want is a list where the reference allele is below the MAF threshold for all pops and the ALT allele is below MAF threshold for all pops
 
+# get sites where the reference allele is the minor allele and below the threshold (they sum to 1, so if it is below the threshold, it is also by definition, the minor allele)
+cbp_refm <- cbp_var[which(cbp_var$REF_FREQ < 0.025),]
+cr_refm <- cr_var[which(cr_var$REF_FREQ < 0.025),]
+cg_refm <- cg_var[which(cg_var$REF_FREQ < 0.05),]
+
+# Then find the shared sites among all three species where the reference allele does not meet the minor allele freq threshold
+refm_shared <- reduce(list(cbp_refm[1:2], cr_refm[1:2], cg_refm[1:2]),
+                    inner_join)
+# Okay, now we can do the same for the sites where the ALT allele does not meet the MAF threshold
+cbp_altm <- cbp_var[which(cbp_var$ALT_FREQ < 0.025),]
+cr_altm <- cr_var[which(cr_var$ALT_FREQ < 0.025),]
+cg_altm <- cg_var[which(cg_var$ALT_FREQ < 0.05),]
+
+#shared 
+altm_shared <- reduce(list(cbp_altm[1:2], cr_altm[1:2], cg_altm[1:2]),
+                      inner_join)
+
+# concatenate reference and alt removals together
+rm_sites <- rbind(altm_shared, refm_shared)
+
+# should be mathematically impossible for there to be duplicates here so no need to worry about that.
 # Write to file
-write.table(rm_sites_nodups[1:2], file = "remove_MAF.txt", sep = "\t", header = F, quote = F, row.names = F, col.names = F)
+write.table(rm_sites, file = paste0(workdir,"/remove_MAF.txt"), sep = "\t", quote = F, row.names = F, col.names = F)
